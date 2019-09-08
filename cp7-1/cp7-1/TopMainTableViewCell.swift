b588f8529aeaeed3c98720ca54cf5f91edfb5f4c
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-08 12:01:20 +0900

    00001

---
+++ cp7-1/cp7-1/TopMainTableViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  TopMainTableViewCell.swift
+//  cp7-1
+//
+//  Created by 酒井典昭 on 2019/09/08.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+protocol TopMainTableViewCellDelegate: NSObjectProtocol{
+    
+}
+
+extension TopMainTableViewCellDelegate {
+    
+}
+// MARK: - Property
+class TopMainTableViewCell: BaseTableViewCell {
+    weak var delegate: TopMainTableViewCellDelegate? = nil
+    @IBOutlet weak var userIcon: UIImageView!
+    
+}
+
+// MARK: - Life cycle
+extension TopMainTableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setIcon()
+    }
+}
+
+// MARK: - Protocol
+extension TopMainTableViewCell {
+    
+}
+
+// MARK: - method
+extension TopMainTableViewCell {
+    func setIcon(){
+      userIcon.layer.cornerRadius = 25
+        userIcon.layer.masksToBounds = true
+    }
+    
+}
+
