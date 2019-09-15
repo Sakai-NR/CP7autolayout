ba49788de242a9023ebca87261f75ee6f64938c1
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-15 23:24:03 +0900

    00003

---
+++ cp7-3/Pods/Target Support Files/Pods-cp7-3/Pods-cp7-3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cp7_3 : NSObject
+@end
+@implementation PodsDummy_Pods_cp7_3
+@end
---
+++ cp7-3/cp7-3/TopMainTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  TopMainTableViewCell.swift
+//  cp7-3
+//
+//  Created by 酒井典昭 on 2019/09/15.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+protocol TopMainTableViewCellDelegate: NSObjectProtocol{
+    
+}
+
+extension TopMainTableViewCellDelegate {
+    
+}
+// MARK: - Property
+class TopMainTableViewCell: BaseTableViewCell {
+    weak var delegate: TopMainTableViewCellDelegate? = nil
+    @IBOutlet weak var userIcon: UIImageView!
+}
+
+// MARK: - Life cycle
+extension TopMainTableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        userset()
+    }
+}
+
+// MARK: - Protocol
+extension TopMainTableViewCell {
+    
+}
+
+// MARK: - method
+extension TopMainTableViewCell {
+    func userset(){
+        userIcon.layer.cornerRadius = 25
+        userIcon.layer.masksToBounds = true
+    }
+}
+
